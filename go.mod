f4b96ea0919920d6847ee994c921590e05b1bea8
AFloresc <afloresc@edu.tecnocampus.cat>
2025-11-11 17:16:07 +0100

    Added server logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module broadcast-server
 
 go 1.24.3
+
+require github.com/gorilla/websocket v1.5.3
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,84 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type Hub struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+type Client struct {
+	conn *websocket.Conn
+	send chan []byte
+}
+
+func ServeWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	client := &Client{conn: conn, send: make(chan []byte, 256)}
+	hub.register <- client
+
+	go client.writePump()
+	go client.readPump(hub)
+}
+
+func (c *Client) readPump(hub *Hub) {
+	defer func() {
+		hub.unregister <- c
+		c.conn.Close()
+	}()
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		hub.broadcast <- message
+	}
+}
+
+func (c *Client) writePump() {
+	for msg := range c.send {
+		c.conn.WriteMessage(websocket.TextMessage, msg)
+	}
+}
