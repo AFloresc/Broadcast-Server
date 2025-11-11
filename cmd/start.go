76057235631d28e857f7589f65f69d834db616bb
AFloresc <afloresc@edu.tecnocampus.cat>
2025-11-11 17:17:51 +0100

    Added StartSerber command

--- cmd/start.go
+++ cmd/start.go
@@ -1,1 +1,19 @@
 package cmd
+
+import (
+	"broadcast-server/internal/server"
+	"log"
+	"net/http"
+)
+
+func StartServer() {
+	hub := server.NewHub()
+	go hub.Run()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		server.ServeWs(hub, w, r)
+	})
+
+	log.Println("Servidor escuchando en :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
