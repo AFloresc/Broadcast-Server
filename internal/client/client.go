a15656b97ef0a6bf21165160f7a7eb1b6e5bfc02
AFloresc <afloresc@edu.tecnocampus.cat>
2025-11-11 17:16:53 +0100

    Added client logic

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,1 +1,35 @@
 package client
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/gorilla/websocket"
+)
+
+func Run(url string) {
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		fmt.Println("Error de conexión:", err)
+		return
+	}
+	defer conn.Close()
+
+	go func() {
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+			fmt.Println("📨", string(msg))
+		}
+	}()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Escribe mensajes para enviar:")
+	for scanner.Scan() {
+		text := scanner.Text()
+		conn.WriteMessage(websocket.TextMessage, []byte(text))
+	}
+}
